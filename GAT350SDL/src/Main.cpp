2a0d753d2f44a11c96d31acdd2d52dd5f7889d2e
AlanBuechner <47373257+AlanBuechner@users.noreply.github.com>
2021-10-06 10:57:21 -0600

    basic rendering

---
+++ GAT350SDL/src/Main.cpp
@@ -0,0 +1,41 @@
+#include "Renderer.h"
+#include "Framebuffer.h"
+
+#include <iostream>
+#include <SDL.h>
+
+int main(int, char**)
+{
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+
+	std::unique_ptr<Renderer> renderer = std::make_unique<Renderer>();
+	renderer->Initialize(WIDTH, HEIGHT);
+
+	std::unique_ptr<Framebuffer> frameBuffer = std::make_unique<Framebuffer>(renderer.get(), WIDTH, HEIGHT);
+	
+	bool quit = false;
+	SDL_Event event;
+	while (!quit)
+	{
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+
+		frameBuffer->Clear({ 0,0,0,0 });
+		frameBuffer->DrawRect(10, 10, 100, 100, {255,255,255,255});
+		frameBuffer->DrawLine(200, 200, 300, 400, { 0,255,0,255 });
+		frameBuffer->Update();
+
+		renderer->CopyBuffer(frameBuffer.get());
+		renderer->Present();
+	}
+
+	SDL_Quit();
+
+	return 0;
+}
---
+++ GAT350SDL/src/Renderer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "SDL.h"
+#include <memory>
+
+using color_t = SDL_Color;
+
+class Framebuffer;
+
+class Renderer
+{
+public:
+	~Renderer();
+
+	bool Initialize(int width, int height, bool fullscreen = false);
+	void CopyBuffer(Framebuffer* buffer);
+	void Present();
+
+public:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	int width = 0;
+	int height = 0;
+};
